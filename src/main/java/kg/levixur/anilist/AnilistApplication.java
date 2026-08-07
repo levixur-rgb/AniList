ca137b5403adfc9a3e3f4888035ba918eefe3e5f
levixur-rgb <levixur@gmail.com>
2026-08-07 17:20:30 +0600

    add hello controller

---
+++ src/main/java/kg/levixur/anilist/AnilistApplication.java
@@ -0,0 +1,13 @@
+package kg.levixur.anilist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnilistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnilistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/levixur/anilist/AnilistApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.levixur.anilist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnilistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
